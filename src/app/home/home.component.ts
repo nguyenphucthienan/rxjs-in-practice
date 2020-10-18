45838a1f9317e4f84ea8e6d63cd82c30647363fc
An Phuc Thien Nguyen <npta97@gmail.com>
2020-10-18 15:38:31 +0700

    Update Home component with imperative design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {createHttpObservable} from '../common/util';
+import {map} from 'rxjs/operators';
+import {Course} from '../model/course';
+import {noop} from 'rxjs';
 
 @Component({
   selector: 'home',
@@ -7,10 +11,26 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  beginnerCourses: Course[] = [];
+  advancedCourses: Course[] = [];
+
   constructor() {
   }
 
   ngOnInit() {
+    const http$ = createHttpObservable('/api/courses');
+    const courses$ = http$.pipe(
+      map(res => res['payload'])
+    );
+
+    courses$.subscribe(
+      courses => {
+        this.beginnerCourses = courses.filter(course => course.category === 'BEGINNER');
+        this.advancedCourses = courses.filter(course => course.category === 'ADVANCED');
+      },
+      noop,
+      () => console.log('Completed')
+    );
   }
 
 }
